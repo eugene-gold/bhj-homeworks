c7123a41ac9582876baf5944f5b7fe4b6dbef451
eugene-gold <uran2@bk.ru>
2022-07-18 08:57:37 +0800

    slider added

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,22 @@
-const menuItem = document.querySelectorAll('.menu__link')
+// const menuItem = document.querySelectorAll('.menu__link')
+//
+// menuItem.forEach(item => item.addEventListener('click', (event) => {
+//     const subMenu = item.closest('.menu__item').querySelector('.menu_sub');
+//     if(subMenu) {
+//         event.preventDefault()
+//         subMenu.classList.toggle('menu_active')
+//     }
+//
+// }))
+//
 
-menuItem.forEach(item => item.addEventListener('click', (event) => {
-    const subMenu = item.closest('.menu__item').querySelector('.menu_sub');
-    if(subMenu) {
-        event.preventDefault()
-        subMenu.classList.toggle('menu_active')
-    }
+const allLinks = document.querySelectorAll('.menu__link')
 
-}))
-
+allLinks.forEach((link) => {
+    link.onclick = function (event) {
+        if (link.closest('.menu__item').querySelector('.menu_sub')) {
+            link.closest('.menu__item').querySelector('.menu_sub').classList.toggle('menu_active');
+            return false
+        }
+    };
+});
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,11 +1,34 @@
-const arrows = document.querySelectorAll('.slider__arrow ');
-const itemsImg = document.querySelectorAll('.slider__item')
-console.log(itemsImg)
+const arrows = document.querySelectorAll('.slider__arrow');
+const itemsImg = Array.from(document.querySelectorAll('.slider__item'))
 
-arrows.forEach(item => item.addEventListener('click', changeImage ))
-
-function changeImage () {
-    let slide = 0;
-    console.log('click')
+function toTheRight (arr) {
+    let curr = arr.findIndex( item => item.classList.contains('slider__item_active'))
+    arr[curr].classList.remove('slider__item_active')
+    if (curr + 1 > arr.length - 1 ) {
+        arr[0].classList.add('slider__item_active')
+    } else {
+        arr[curr + 1].classList.add('slider__item_active')
+    }
 }
+
+function toTheLeft (arr) {
+    let curr = arr.findIndex( item => item.classList.contains('slider__item_active'))
+    arr[curr].classList.remove('slider__item_active')
+    if (curr - 1 < 0 ) {
+        arr[arr.length - 1].classList.add('slider__item_active')
+    } else {
+        arr[curr - 1].classList.add('slider__item_active')
+    }
+}
+
+arrows.forEach(item => item.addEventListener('click', () => {
+    if (item.classList.contains('slider__arrow_next')) {
+        toTheRight(itemsImg)
+    }
+    if (item.classList.contains('slider__arrow_prev')) {
+        toTheLeft(itemsImg)
+    }
+} ))
+
+
 
